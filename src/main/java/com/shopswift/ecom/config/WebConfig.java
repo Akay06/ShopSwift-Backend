21192ce51e418915605d2338deebac2ccecfc67e
Kannappan Aravind Raghavan <72030643+Akay06@users.noreply.github.com>
2025-02-09 16:44:55 -0600

    Update WebConfig.java

--- src/main/java/com/shopswift/ecom/config/WebConfig.java
+++ src/main/java/com/shopswift/ecom/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 			//allowedOrigins("http://localhost:8000")
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://akay06.github.io")
+				registry.addMapping("/**").allowedOrigins("https://shopswift.akay06.com")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
 						.allowCredentials(true).maxAge(3600); // Cache preflight response for 1 hour
 			}
